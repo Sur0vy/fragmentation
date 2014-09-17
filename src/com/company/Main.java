26bdcf9973cc13b8e48e4814a3b9fb4095ac6daa
Sur0vy <Sur0vy@yandex.ru>
2014-09-17 14:52:02 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int MAX_NUMBER = 150;
+        System.out.print("Enter \"n\" and \"k\":");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        if ((n >= MAX_NUMBER) && (k >= MAX_NUMBER)){
+            System.out.println("Input values can't be bigger than " + MAX_NUMBER);
+        } else {
+            int cnt = FragmentationCount.split(n, k);
+            System.out.println("Fragmentation count = " + cnt);
+        }
+    }
+}
