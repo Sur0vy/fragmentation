99f275eac7aee85d69e13bbbad6c6566ad469bca
Sur0vy <Sur0vy@yandex.ru>
2014-09-18 12:24:41 +0400

    добавлена проверка правильности ввода значений

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,23 @@ public class Main {
         final int MAX_NUMBER = 150;
         System.out.print("Enter \"n\" and \"k\":");
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        if ((n > MAX_NUMBER) || (k > MAX_NUMBER)){
-            System.out.println("Input values can't be bigger than " + MAX_NUMBER);
-        } else {
-            int cnt = FragmentationCount.split(n, k);
-            System.out.println("Fragmentation count = " + cnt);
+        int n;
+        int k;
+        try {
+            n = in.nextInt();
+            k = in.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid values!");
+            n = 0;
+            k = 0;
+        }
+        if ((n > 0) && (k > 0)) {
+            if ((n > MAX_NUMBER) || (k > MAX_NUMBER)) {
+                System.out.println("Input values can't be bigger than " + MAX_NUMBER);
+            } else {
+                int cnt = FragmentationCount.split(n, k);
+                System.out.println("Fragmentation count = " + cnt);
+            }
         }
     }
 }
